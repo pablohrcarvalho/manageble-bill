e0b702f02022468cec1c6194bad4cd6669774695
pablohrcarvalho <45HG3g7thg!>
2021-12-11 14:19:42 -0300

    Configurando projeto inicial

---
+++ src/main/java/com/fornow/manageblebill/ManagebleBillApplication.java
@@ -0,0 +1,13 @@
+package com.fornow.manageblebill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagebleBillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagebleBillApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fornow/manageblebill/ManagebleBillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fornow.manageblebill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagebleBillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
